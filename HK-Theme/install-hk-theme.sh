50ac45c566cba9649f0612100d78705fd9b7237c
DimasDFAR <aryasatyadimasfebri123@gmail.com>
2025-12-04 11:41:51 +0700

    renamed the dotfile dir

--- HK-Theme/install-hk-theme.sh
+++ HK-Theme/install-hk-theme.sh
@@ -2,9 +2,9 @@
 set -e
 
 # Absolute paths
-THEME_DIR="~/dotfiles/HK-Theme"
-FONT_DIR="~/dotfiles/fonts"
-WALLPAPER="~/dotfiles/HK-Theme/wallpapers/HollowKnight.jpg"
+THEME_DIR="~/KDE-​Game​Theme​Dotfiles/HK-Theme"
+FONT_DIR="~/KDE-​Game​Theme​Dotfiles/fonts"
+WALLPAPER="~/KDE-​Game​Theme​Dotfiles/HK-Theme/wallpapers/HollowKnight.jpg"
 
 echo "Installing MesloLGL Nerd Fonts system-wide..."
 sudo mkdir -p /usr/share/fonts/truetype/meslo
@@ -17,26 +17,31 @@ sudo fc-cache -f -v
 echo "Copying theme configs..."
 
 # --- Zsh ---
-echo "Applying Zsh config..."
+echo "Applying Zsh configs..."
 cp ~/dotfiles/HK-Theme/.zshrc ~/.zshrc
 
 # Color schemes
-mkdir -p "$HOME/.local/share/color-schemes"
-cp -r "~/dotfiles/HK-Theme/color-schemes"* "$HOME/.local/share/color-schemes/"
-plasma-apply-colorscheme "ArcDark"
+echo "Applying color schemes..."
+mkdir -p ~/.local/share/color-schemes
+cp -r ~/KDE-GameThemeDotfiles/HK-Theme/color-schemes/ArcDark.colors ~/.local/share/color-schemes/ArcDark.colors
+plasma-apply-colorscheme "Arc Dark"
 
 # Fastfetch
-mkdir -p "$HOME/.config/fastfetch"
-cp -r "~/dotfiles/HK-Theme/fastfetch"* "$HOME/.config/fastfetch/"
+echo "Applying fastfetch configs..."
+mkdir -p ~/.config/fastfetch
+cp -r ~/KDE-GameThemeDotfiles/HK-Theme/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc
 
 # Starship
-mkdir -p "$HOME/.config/starship"
-cp -r "~/dotfiles/HK-Theme/starship"* "$HOME/.config/starship/"
+echo "Applying starship configs..."
+mkdir -p ~/.config/starship
+cp -r ~/KDE-GameThemeDotfiles/HK-Theme/starship/starship.toml ~/.config/starship/starship.toml
 
 # Konsole
-mkdir -p "$HOME/.local/share/konsole"
-cp -r "~/dotfiles/HK-Theme/konsole"* "$HOME/.local/share/konsole/"
-cp ~/dotfiles/HK-Theme/konsole ~/.config/konsolerc
+echo "Applying Konsole configs..."
+mkdir -p ~/.local/share/konsole
+cp -r ~/KDE-GameThemeDotfiles/HK-Theme/konsole/HollowKnight.profile ~/.local/share/konsole/HollowKnight.profile
+cp -r ~/KDE-GameThemeDotfiles/HK-Theme/konsole/Nordic.colorscheme ~/.local/share/konsole/Nordic.colorscheme
+cp -r ~/KDE-GameThemeDotfiles/HK-Theme/konsolerc ~/.config/konsolerc
 
 # Plasma panel/layout
 cp "~/dotfiles/HK-Theme/plasma/plasma-org.kde.plasma.desktop-appletsrc" "$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc"
@@ -58,4 +63,4 @@ echo "Reloading Plasma to apply changes..."
 kquitapp5 plasmashell || true
 plasmashell &
 
-echo "HK-Theme installed and applied successfully!"
+echo "Hollow Knight theme applied! (You may need to restart for all changes to apply)"
--- HK-Theme/konsole/HollowKnight.profile
+++ HK-Theme/konsole/HollowKnight.profile
@@ -7,7 +7,7 @@ WordMode=false
 [General]
 Command=/usr/bin/zsh
 Environment=TERM=xterm-256color,COLORTERM=truecolor
-Icon=~/dotfiles/HK-Theme/fastfetch/Hollow-Knight-PNG-Clipart.png
+Icon=~/KDE-GameThemeDotfiles/HK-Theme/fastfetch/Hollow-Knight-PNG-Clipart.png
 Name=HollowKnight
 Parent=FALLBACK/
 TerminalCenter=false
